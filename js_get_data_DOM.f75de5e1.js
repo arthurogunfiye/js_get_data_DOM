148d5dc070d41d16ccbc32a0c1bef2c79317ea72
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-04-26 17:24:01 +0100

    make build

---
+++ js_get_data_DOM.f75de5e1.js
@@ -0,0 +1,12 @@
+'use strict';
+const populationElements = document.querySelectorAll('.population');
+const populationNumbers = [];
+for(let i = 0; i < populationElements.length; i++)populationNumbers.push(+populationElements[i].innerText.replaceAll(',', ''));
+const total = populationNumbers.reduce((acc, curr)=>acc + curr, 0);
+const average = Math.floor(total / populationNumbers.length);
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+totalElement.innerText = total.toLocaleString();
+averageElement.innerText = average.toLocaleString();
+
+//# sourceMappingURL=js_get_data_DOM.f75de5e1.js.map
